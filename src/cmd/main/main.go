be43f1d3c7094822813a40e87905f12344abaaf8
Arnaud <arnaudralec@gmail.com>
2024-09-14 09:23:04 +1100

    💡 chore: update comments

--- src/cmd/main/main.go
+++ src/cmd/main/main.go
@@ -5,8 +5,8 @@ import (
 	"wails-templ-htmx-project/src/cmd/render"
 )
 
-// main runs the core application (server-side code + templating)
+// main runs the core application (server-side code API)
 func main() {
-	r := render.GetRenderingMux()
+	r := render.GetRenderingMux() // TODO : main should only serve the core backend API. New adapters must be added to the repository.
 	http.ListenAndServe(":8080", r)
 }
